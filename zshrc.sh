0546120f18f8309b1c681a3a990fff989e6f75cc
Cayce House <cayce@mobilemedics.com>
2020-07-30 13:14:55 -0400

    Replaced docker with valet

--- zshrc.sh
+++ zshrc.sh
@@ -1,17 +1,20 @@
-# Check if zplug is installed
+# Check if zplug is installed.
 if [[ ! -d ~/.zplug ]]; then
   git clone https://github.com/zplug/zplug ~/.zplug
   source ~/.zplug/init.zsh && zplug update --self
 fi
 
-# Essential
+# Essential.
 source ~/.zplug/init.zsh
 
 # Keybindings for substring search plugin. Maps up and down arrows.
 bindkey -M main '^[[A' history-substring-search-up
 bindkey -M main '^[[B' history-substring-search-up
 
-# Make sure to use double quotes to prevent shell expansion
+# Add composer's binfiles to path.
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+
+# Make sure to use double quotes to prevent shell expansion.
 zplug "woefe/wbase.zsh"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
@@ -19,7 +22,7 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search", defer:3
 zplug "dracula/zsh", as:theme
 
-# Install packages that have not been installed yet
+# Install packages that have not been installed yet.
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
     if read -q; then
