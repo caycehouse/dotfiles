bb568730b62ef6d34984e4f3d028c90f9b8fd69c
Cayce House <cayce@caycehouse.com>
2024-06-04 11:42:50 -0400

    correct ps1

--- README.md
+++ README.md
@@ -4,9 +4,17 @@ My dotfiles managed by chezmoi
 
 ## first time set up
 
+### Bash
 ```sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/caycehouse/dotfiles/HEAD/install.sh)"
 ```
+
+### Powershell
+```ps1
+. { iwr -useb https://raw.githubusercontent.com/caycehouse/dotfiles/HEAD/install.ps1 } | iex
+```
+
+
 ## gnome extensions to install:
 https://nightthemeswitcher.romainvigier.fr/
 https://gitlab.com/asus-linux/supergfxctl-gex
--- install.ps1
+++ install.ps1
@@ -1,23 +1,27 @@
 # Set the execution policy to allow scripts to run
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
 
-Write-Host "`🤚  This script will setup .dotfiles for you.`"
-Read-Host -n 1 -r -s -p "    Press any key to continue or Ctrl+C to abort...\n\n"
+# Install latest Powershell version
+winget install --id Microsoft.Powershell --source winget
+
+Write-Host ""
+Write-Host "🤚  This script will setup .dotfiles for you."
+Read-Host "    Press any key to continue or Ctrl+C to abort...`n`n"
 
 # Install chezmoi
-if (!(Get-Package -Name chezmoi)) {
-  Write-Host "`👊  Installing chezmoi"
+if (!(Get-Command -Name chezmoi)) {
+  Write-Host "👊  Installing chezmoi"
   winget install chezmoi
 }
 
 $chezmoiDir = "$HOME\.local\share\chezmoi\.git"
 if (Test-Path $chezmoiDir) {
-  Write-Host "`🚸  chezmoi already initialized"
+  Write-Host "🚸  chezmoi already initialized"
   Write-Host "    Reinitialize with: 'chezmoi init git@github.com:caycehouse/dotfiles.git'"
 } else {
-  Write-Host "`🚀  Initialize dotfiles with:"
+  Write-Host "🚀  Initialize dotfiles with:"
   Write-Host "    chezmoi init git@github.com:caycehouse/dotfiles.git"
 }
 
-Write-Host "`"
+Write-Host ""
 Write-Host "Done."
