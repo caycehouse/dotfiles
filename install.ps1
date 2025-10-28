6738ff34cb5aa4d1638cac4321227fb266be73df
Cayce House <cayce@caycehouse.com>
2025-10-28 11:41:57 -0400

    chore: cleanup install.ps1

--- install.ps1
+++ install.ps1
@@ -4,24 +4,14 @@ Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
 # Install latest Powershell version
 winget install --id Microsoft.Powershell --source winget
 
-Write-Host ""
-Write-Host "🤚  This script will setup .dotfiles for you."
-Read-Host "    Press any key to continue or Ctrl+C to abort...`n`n"
-
-# Install chezmoi
-if (!(Get-Command -Name chezmoi)) {
-  Write-Host "👊  Installing chezmoi"
+# Install chezmoi if necessary
+if (Get-Command -Name chezmoi) {
+  Write-Host "Chezmoi is already installed."
+} else {
   winget install chezmoi
 }
 
-$chezmoiDir = "$HOME\.local\share\chezmoi\.git"
-if (Test-Path $chezmoiDir) {
-  Write-Host "🚸  chezmoi already initialized"
-  Write-Host "    Reinitialize with: 'chezmoi init git@github.com:caycehouse/dotfiles.git'"
-} else {
-  Write-Host "🚀  Initialize dotfiles with:"
-  Write-Host "    chezmoi init git@github.com:caycehouse/dotfiles.git"
-}
-
-Write-Host ""
-Write-Host "Done."
+# Apply dotfiles
+Write-Host "Applying Chezmoi configuration."
+chezmoi init caycehouse/dotfiles
+chezmoi apply
